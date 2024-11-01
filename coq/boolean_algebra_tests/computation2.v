8116d299e1bee00b68d5de7f56e31a7d022033f9
noele2 <nobodiz123@gmail.com>
2024-10-31 23:57:23 -0500

    simplify comp2

--- coq/boolean_algebra_tests/computation2.v
+++ coq/boolean_algebra_tests/computation2.v
@@ -1,21 +1,4 @@
-(* Define Boolean values *)
-Inductive bool : Type :=
-  | true : bool
-  | false : bool.
-
-(* Define addition (OR) and multiplication (AND) for booleans *)
-Definition boolean_add (b1 b2 : bool) : bool :=
-  match b1, b2 with
-  | true, _ => true
-  | _, true => true
-  | false, false => false
-  end.
-
-Definition boolean_mult (b1 b2 : bool) : bool :=
-  match b1, b2 with
-  | true, true => true
-  | _, _ => false
-  end.
+Require Import boolean_ops. 
 
 (* Lemma to prove the computation *)
 Lemma computation2 : forall A B C P Q : bool,
